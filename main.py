7d9807854206cf887f0965d8cf677097c282ccfe
juan david santamaria henao <juan@MacBook-Air-de-juan.local>
2026-03-09 08:09:07 -0500

    middleware

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ from core.database import Base, engine
 from models.user import User
 from models.role import Role
 from routes.auth import router as auth_router
+from middlewares.cors import add_cors_middleware
 
 app = FastAPI()
+
+# Configurar CORS desde la carpeta middlewares
+add_cors_middleware(app)
 
 Base.metadata.create_all(bind=engine)
 
